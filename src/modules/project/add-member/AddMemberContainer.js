a88ab312db5f0543ef706f59ad2398a932812c76
giorgiana_p <giorgiana.piscoi@evozon.com>
2016-12-13 11:56:05 +0200

    added members autocomplete component

---
+++ src/modules/project/add-member/AddMemberContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+class AddMemberContainer extends Component { // eslint-disable-line
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(elms) {
+    console.log('eeeee', elms);
+    this.setState({ values: elms });
+  }
+  render() {
+    const isMulti = true;
+    const options = [
+      { value: 'one', label: 'One' },
+      { value: 'two', label: 'Two1' },
+      { value: 'three', label: 'Three' },
+      { value: 'four', label: 'Four' },
+      { value: 'five', label: 'Two4' },
+      { value: 'six', label: 'Two5' },
+      { value: 'seven', label: 'Seven' },
+      { value: 'eight', label: 'Eight' },
+    ];
+    return (
+      <Select
+        name="form-field-name"
+        multi={isMulti}
+        options={options}
+        onChange={this.handleChange}
+        value={this.state.values}
+        placeholder={'Select members'}
+      />
+    );
+  }
+}
+
+export default AddMemberContainer;
--- src/modules/project/components/Project.js
+++ src/modules/project/components/Project.js
@@ -1,30 +1,46 @@
-import React, { PropTypes } from 'react';
-import { ListGroupItem, Tag, Button, Collapse  } from 'reactstrap';
+import React, { Component, PropTypes } from 'react';
+import { ListGroupItem, Tag, Button, Collapse } from 'reactstrap';
+import AddMemberContainer from 'modules/project/add-member/AddMemberContainer';
 
 const propTypes = {
   info: PropTypes.string,
 };
 
-function Project({ info }) {
-  return (
-    <ListGroupItem>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3">
-            <strong>{info} </strong>
-          </div>
-          <div className="col-md-3">
-            <Tag color="primary" pill>2</Tag> devices
-          </div>
-          <div className="col-md-3">
-            <Tag color="primary" pill>12</Tag> members
-          </div>
-          <div className="col-md-3">
-            <Button color="primary">+</Button>
+class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+  render() {
+    return (
+      <ListGroupItem>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <strong>{this.props.info} </strong>
+            </div>
+            <div className="col-md-3">
+              <Tag color="primary" pill>2</Tag> devices
+            </div>
+            <div className="col-md-3">
+              <Tag color="primary" pill>12</Tag> members
+              <Collapse isOpen={this.state.isOpen}>
+                <AddMemberContainer />
+              </Collapse>
+            </div>
+            <div className="col-md-3">
+              <Button color="primary" onClick={this.toggle}>+</Button>
+            </div>
           </div>
         </div>
-      </div>
-    </ListGroupItem>);
+      </ListGroupItem>);
+  }
 }
 
 Project.propTypes = propTypes;
